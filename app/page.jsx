2fb60f99899603a4d9e86f94ec635c5005fbfe25
Rajesh Choudhary <rajeshchoudharyt@gmail.com>
2025-09-02 18:41:22 +0530

    init

---
+++ app/page.jsx
@@ -0,0 +1,8 @@
+export default function Home() {
+    return (
+        <div className="font-sans">
+            <p>Hello</p>
+            <p className="font-mono">World</p>
+        </div>
+    );
+}
